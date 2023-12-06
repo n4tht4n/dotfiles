690b2e71a39c060b1757a907ff8c72af0013b092
N4tht4N <n4tht4n@mailbox.org>
2023-12-06 22:21:17 +0100

    Use fancy backdrop with wezterm
    However, you have to provide a `backdrop.jpg` image for yourself.
    
    Create a `backdrops` subfolder in `$XDG_CONFIG_HOME/wezterm`, put your image
    inside that folder and name it `backdrop.jpg` (or reconfigure it), and you're
    ready to go.

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,13 +1,21 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- see: https://github.com/Tony-Sol/.config/blob/master/wezterm/wezterm.lua
+-- crazy wezterm config: https://github.com/KevinSilvester/wezterm-config
 
 return {
-  term = 'wezterm',
+  term = "wezterm",
+
+  animation_fps = 60,
+  max_fps = 60,
+  front_end = "WebGpu",
+  webgpu_power_preference = "LowPower",
+
   -- default_prog  = { "/bin/zsh", "-l", "-c", "tmux attach || tmux" },
-  color_scheme = 'Dracula (Official)',
+  color_scheme = "Dracula (Official)",
   unicode_version = 14,
-  font = wezterm.font_with_fallback {
+
+  font = wezterm.font_with_fallback({
     {
       family = "Fira Code",
       weight = "Regular",
@@ -17,22 +25,22 @@ return {
         "clig=1",
         "liga=1",
         -- stylistic sets: https://github.com/tonsky/FiraCode/wiki/How-to-enable-stylistic-sets
-        'cv32', -- .=
-        'cv28', -- special . with curly braces
-        'ss06', -- \\
-        'ss07', -- =~ !~
-        'ss10', -- Fl Tl fi fj fl ft
-        'ss09', -- >>= <<= ||= |=
+        "cv32", -- .=
+        "cv28", -- special . with curly braces
+        "ss06", -- \\
+        "ss07", -- =~ !~
+        "ss10", -- Fl Tl fi fj fl ft
+        "ss09", -- >>= <<= ||= |=
       },
-     assume_emoji_presentation = false,
+      assume_emoji_presentation = false,
     },
     {
-      family = "Hack Nerd Font Mono",
+      family = "Hack Nerd Font",
       weight = "Regular",
       harfbuzz_features = { "calt=1", "clig=1", "liga=1" },
       assume_emoji_presentation = true,
     },
-  },
+  }),
   font_size = 20.0,
   initial_cols = 150,
   initial_rows = 42,
@@ -42,5 +50,18 @@ return {
   hide_tab_bar_if_only_one_tab = true,
 
   window_background_opacity = 0.95,
+  window_close_confirmation = "NeverPrompt",
   window_decorations = "RESIZE",
+
+  background = {
+    {
+      source = { File = wezterm.config_dir .. "/backdrops/backdrop.jpg" },
+    },
+    {
+      source = { Color = "black" },
+      height = "100%",
+      width = "100%",
+      opacity = 0.90,
+    },
+  },
 }
